c45885581eb765ae7af50ec3a6eb8c4ee00f34be
Suki <tues1205@naver.com>
2024-11-01 15:38:32 +0900

    코드문제 삽입

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './routes/Home';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./routes/Home";
+import ErrorGame from "./routes/ErrorGame";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/findError" element={<ErrorGame />} />
       </Routes>
     </>
   );
