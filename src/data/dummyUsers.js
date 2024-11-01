b563b6c7ce43b7461f6b72e226096c621fd64ad7
Yuseong Na <us9567@naver.com>
2024-11-02 03:26:39 +0900

    로그인 기능 추가

---
+++ src/data/dummyUsers.js
@@ -0,0 +1,31 @@
+const dummyUsers = [
+  {
+    id: "ckals413",
+    password: "1234",
+    skills: ["Kotlin", "C", "React"], // 구현 가능 기술
+    badges: ["Gold", "Silver", "1달 연속 접속"], // 뱃지
+    githubId: "ckals413-github", // GitHub ID
+    profileImage:
+      "https://img.freepik.com/premium-photo/maltese-domestic-dog-puppy-running-across-meadow-rhinelandpalatinate_905081-313.jpg", // 프로필 이미지
+  },
+  {
+    id: "f",
+    password: "f",
+    skills: ["JavaScript", "React", "Node.js"],
+    badges: ["Silver", "1달 연속 접속"],
+    githubId: "ffffff-github",
+    profileImage:
+      "https://flexible.img.hani.co.kr/flexible/normal/970/777/imgdb/resize/2019/0926/00501881_20190926.JPG", // 프로필 이미지
+  },
+  {
+    id: "d",
+    password: "d",
+    skills: ["Python", "Django", "Machine Learning"],
+    badges: ["Bronze", "1주 연속 접속"],
+    githubId: "testuser3-github",
+    profileImage:
+      "https://media.istockphoto.com/id/1143958799/ko/%EC%82%AC%EC%A7%84/%ED%9D%B0%EC%83%89-%EC%A0%88%EC%97%B0-%EC%95%85%EC%96%B4.jpg?s=612x612&w=0&k=20&c=BDnKV9wf0qa4F-G6zvTOG1shW5YYKCCd9indY6R_DjY=", // 프로필 이미지
+  },
+];
+
+export default dummyUsers;
