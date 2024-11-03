f37c60d1d542a8204c90e51fddf0d81e710e6f6d
Yuseong Na <us9567@naver.com>
2024-11-03 20:06:50 +0900

    게시글 목록에서 시간 반영

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -7,13 +7,13 @@ import timeAgo from "../utils/timeAgo";
 const BoardCard = ({ post }) => (
   <div className="board-card-wrapper">
     <Link to={`/board/post/${post.id}`} className="board-card-link">
-      {" "}
-      {/* 절대 경로로 수정 */}
       <div className="board-card">
         <h3>{post.title}</h3>
         <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
         <div className="card-footer">
-          <span>{post.author}</span> • <span>{timeAgo(post.createdAt)}</span>
+          <span>{post.author}</span> •{" "}
+          <span>{timeAgo(new Date(post.createdAt))}</span>
+          {/* Date 객체로 변환하여 전달 */}
         </div>
       </div>
     </Link>
