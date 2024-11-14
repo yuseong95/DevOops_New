e71c0eca64952958efce8de2bc962a0a8a29164e
Suki <tues1205@naver.com>
2024-11-15 01:47:06 +0900

    REFACTOR:useReducer() 사용

--- src/routes/ErrorGame.js
+++ src/routes/ErrorGame.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useMemo } from "react";
+import React, { useEffect, useState, useRef, useMemo, useReducer } from "react";
 import { useNavigate } from "react-router-dom";
 import Prism from "prismjs";
 import "./css/ErrorGame.css";
@@ -8,37 +8,74 @@ import "prismjs/components/prism-c.min.js";
 import ErrorGameResult from "../components/ErrorGameResult";
 import Timer from "../components/Timer";
 
+// 초기 상태
+const initState = {
+  showQuiz: false, // 문제 보이기
+  currentFile: 0, // 현재 보여지는 문제의 인덱스(0,1,2)
+  explanationMode: false, // 해설 모드 (초기값 false)
+  explanationIndex: 0, // 현재 보여지는 해설의 인덱스(0,1,2)
+  userAnswers: ["", "", ""], // user가 입력한 3문제의 답
+  resultCount: null, // 맞은 문제 수
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "START": // 게임 시작
+      return { ...state, showQuiz: true, currentFile: 0 };
+    case "RESULT_COUNT": // 맞을 결과 저장
+      return { ...state, resultCount: action.count };
+    case "NEXT_QUIZ": // 다음 문제
+      return { ...state, currentFile: state.currentFile + 1 };
+    case "EXPLANATION_MODE": // 해설 모드
+      return { ...state, explanationMode: true, explanationIndex: 0 };
+    case "NEXT_EXPLANATION": // 다음 해설
+      return { ...state, explanationIndex: state.explanationIndex + 1 };
+    case "UPDATE_ANSWER": // 사용자 답 업데이트
+      const updatedAnswers = [...state.userAnswers];
+      updatedAnswers[state.currentFile] = action.answer;
+      return { ...state, userAnswers: updatedAnswers };
+    default:
+      return state;
+  }
+};
+
+// 오늘의 세트 인덱스 계산 함수
+const calculateTodaySetIndex = () => {
+  const today = new Date();
+  const dayNumber = Math.floor(today.getTime() / (1000 * 60 * 60 * 24));
+  return dayNumber % 7;
+};
+
 const ErrorGame = () => {
-  const [showQuiz, setShowQuiz] = useState(false); // 문제 보이기
+  const [state, dispatch] = useReducer(reducer, initState);
+  const {
+    showQuiz,
+    currentFile,
+    explanationMode,
+    explanationIndex,
+    userAnswers,
+    resultCount,
+  } = state;
+
   const [files, setFiles] = useState([]); // 오늘의 3문제 배열
-  const [, /*currentSetIndex*/ setCurrentSetIndex] = useState(0); // 문제 세트의 인덱스(0~6개 3문제씩 총 21문제)
-  const [currentFile, setCurrentFile] = useState(0); // 현재 보여지는 문제의 인덱스(0,1,2)
   const [fileContent, setFileContent] = useState("READY"); // 문제 파일의 내용
-  const [userAnswers, setUserAnswers] = useState(["", "", ""]); // user가 입력한 3문제의 답
-  const [resultCount, setResultCount] = useState(null); // 맞은 문제 수
-  const [explanationMode, setExplanationMode] = useState(false); // 해설 모드 (초기값 false)
-  const [explanationIndex, setExplanationIndex] = useState(0); // 현재 보여지는 해설의 인덱스(0,1,2)
   const [time, setTime] = useState(null); // 소요 시간
+
   const preRef = useRef(null); // 문제 스크롤바 제어
   const navigate = useNavigate();
+
+  // 현재날짜에 따라 변하므로 useMemo 사용
+  const todaySetIndex = useMemo(() => calculateTodaySetIndex(), []);
+  // file이 변경되지 않으면 바뀌지 않으므로 useMemo 사용
+  const todayAnswers = useMemo(() => files.map((file) => file.answer), [files]);
+  const todayComments = useMemo(
+    () => files.map((file) => file.comment),
+    [files]
+  );
 
   const goToHome = () => {
     navigate("/"); // 메인페이지로 이동
   };
-
-  // 오늘의 세트 인덱스 계산 함수
-  const calculateTodaySetIndex = () => {
-    const today = new Date();
-    const dayNumber = Math.floor(today.getTime() / (1000 * 60 * 60 * 24));
-    return dayNumber % 7;
-  };
-
-  // 현재날짜에 따라 변하므로 useMemo 사용
-  const todaySetIndex = useMemo(() => calculateTodaySetIndex(), []);
-
-  // file이 변경되지 않으면 바뀌지 않으므로 useMemo 사용
-  const answers = useMemo(() => files.map((file) => file.answer), [files]);
-  const comments = useMemo(() => files.map((file) => file.comment), [files]);
 
   // 문제 파일 목록 가져오기
   useEffect(() => {
@@ -51,9 +88,6 @@ const ErrorGame = () => {
 
         const data = await response.json();
         setFiles(data);
-
-        // 오늘의 세트 인덱스 계산
-        setCurrentSetIndex(todaySetIndex);
 
         // 오늘의 세트 (3문제) 가져오기
         const startIndex = todaySetIndex * 3;
@@ -76,7 +110,7 @@ const ErrorGame = () => {
 
   // 문제풀이 시작, 타이머 시작
   const handleStart = () => {
-    setShowQuiz(true);
+    dispatch({ type: "START" });
     loadFileContent(files[0]);
   };
 
@@ -99,11 +133,11 @@ const ErrorGame = () => {
   const checkAnswers = () => {
     const correctCount = userAnswers.reduce(
       (count, answer, index) =>
-        answer.trim() === answers[index] ? count + 1 : count,
+        answer.trim() === todayAnswers[index] ? count + 1 : count,
       0
     );
 
-    setResultCount(correctCount);
+    dispatch({ type: "RESULT_COUNT", count: correctCount });
   };
 
   // 복사 이벤트 방지
@@ -118,9 +152,8 @@ const ErrorGame = () => {
     if (!explanationMode) {
       // 마지막 문제가 아니라면 다음 문제 불러오기
       if (currentFile < 2) {
-        const nextIndex = currentFile + 1;
-        setCurrentFile(nextIndex);
-        loadFileContent(files[nextIndex]);
+        dispatch({ type: "NEXT_QUIZ" });
+        loadFileContent(files[currentFile + 1]);
 
         // <pre> 스크롤바 맨 위로
         if (preRef.current) {
@@ -134,7 +167,7 @@ const ErrorGame = () => {
       // 해설 모드일 경우
 
       if (explanationIndex < 2) {
-        setExplanationIndex((prevIndex) => prevIndex + 1);
+        dispatch({ type: "NEXT_EXPLANATION" });
         loadFileContent(files[explanationIndex + 1]);
 
         // <pre> 스크롤바 맨 위로
@@ -150,15 +183,12 @@ const ErrorGame = () => {
 
   // user의 답 업데이트
   const handleAnswerUpdate = (event) => {
-    const updatedAnswers = [...userAnswers];
-    updatedAnswers[currentFile] = event.target.value;
-    setUserAnswers(updatedAnswers);
+    dispatch({ type: "UPDATE_ANSWER", answer: event.target.value });
   };
 
   // 해설 모드로 바꾸기
   const handleExplanationMode = () => {
-    setExplanationMode(true);
-    setExplanationIndex(0);
+    dispatch({ type: "EXPLANATION_MODE" });
   };
 
   const handleTimer = (time) => {
@@ -213,7 +243,6 @@ const ErrorGame = () => {
             <button
               className="input-button"
               onClick={handleNext}
-              count={resultCount}
               disabled={!showQuiz}
             >
               {currentFile < 2 ? "다음" : "제출"}
@@ -245,8 +274,8 @@ const ErrorGame = () => {
 
           <div className="explanation">
             <p>내 답안: {userAnswers[explanationIndex]}</p>
-            <p>정답: {answers[explanationIndex]}</p>
-            <p>해설: {comments[explanationIndex]}</p>
+            <p>정답: {todayAnswers[explanationIndex]}</p>
+            <p>해설: {todayComments[explanationIndex]}</p>
           </div>
           <button className="input-button" onClick={handleNext}>
             {explanationIndex < 2 ? "다음" : "메인으로"}
