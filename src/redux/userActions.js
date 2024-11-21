726ad04eb86077b4ffcc6f10e8b40c934641e01c
kimdonghyuk <kdh367036@naver.com>
2024-11-21 14:48:51 +0900

    FEAT: 랭킹,스코어redux,점수계산

--- src/redux/userActions.js
+++ src/redux/userActions.js
@@ -1,11 +1,18 @@
-export const UPDATE_SCORE = "UPDATE_SCORE";
-export const RESET_SCORES = "RESET_SCORES";
-export const SYNC_USERS = "SYNC_USERS";
+export const UPDATE_SCORE = 'UPDATE_SCORE';
+export const UPDATE_Typing_Game_SCORE = 'UPDATE_Typing_Game_SCORE';
+export const RESET_SCORES = 'RESET_SCORES';
+export const SYNC_USERS = 'SYNC_USERS';
 
 // 점수 업데이트
 export const updateScore = (userId, score) => ({
   type: UPDATE_SCORE,
   payload: { userId, score },
+});
+
+// 타이핑 챌린지 점수 업데이트
+export const updateTypingGameScore = (userId, typingGameScore) => ({
+  type: UPDATE_Typing_Game_SCORE,
+  payload: { userId, typingGameScore },
 });
 
 // 점수 초기화
