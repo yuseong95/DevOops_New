c0b569d7ca224a11063a6af59dd58f94b4a2ddbf
Suki <tues1205@naver.com>
2024-11-25 02:23:15 +0900

    FEAT: 유형테스트 문제/선택지 보이기

--- src/routes/TypeTest.js
+++ src/routes/TypeTest.js
@@ -1,11 +1,38 @@
 import React, { useState } from "react";
-import "./css/TypeTest.css"; // CSS 파일 분리
+import questionsData from "../data/typeTest.json";
+import "./css/TypeTest.css";
 
 const TypeTest = () => {
-  const [isStart, setIsStart] = useState(true); // start-box 표시
+  const [isStart, setIsStart] = useState(true); // true면 start, false이면 test 화면
+  const [questionIndex, setQuestionIndex] = useState(0); // 현재 질문 인덱스
+  const [typeScores, setTypeScores] = useState(Array(8).fill(0)); // 8개의 유형 점수
 
+  // 시작 버튼 누르면 질문 화면으로
   const handleStartClick = () => {
-    setIsStart(false); // start-box 숨기고 test-box 표시
+    setIsStart(false);
+  };
+
+  // 다음 질문으로
+  const handleNextQuestion = (option) => {
+    const selectedScores =
+      questionsData.questions[questionIndex].options[option].scores;
+
+    // 점수 업데이트
+    // userScores는 비동기 -> 변경된 값을 바로 사용할 수 없어서 로컬 변수 updateScores를 만듦
+    const updatedScores = [...typeScores];
+    selectedScores.forEach((score) => {
+      updatedScores[score - 1] += 1; // 해당 유형에 +1점
+    });
+
+    // 최신점수 저장
+    setTypeScores(updatedScores);
+
+    // 다음 질문으로
+    if (questionIndex < questionsData.questions.length - 1) {
+      setQuestionIndex(questionIndex + 1);
+    } else {
+      console.log("최종 점수:", updatedScores); // 최종 점수
+    }
   };
 
   return (
@@ -24,8 +51,22 @@ const TypeTest = () => {
         ) : (
           <div className="test-box">
             <p className="test-title">
-              테스트를 시작합니다! 질문이 여기에 표시됩니다.
+              {questionsData.questions[questionIndex].question}
             </p>
+            <div className="options-box">
+              <button // A 선택지
+                className="option-button-A"
+                onClick={() => handleNextQuestion("A")}
+              >
+                {questionsData.questions[questionIndex].options.A.text}
+              </button>
+              <button // B 선택지
+                className="option-button-B"
+                onClick={() => handleNextQuestion("B")}
+              >
+                {questionsData.questions[questionIndex].options.B.text}
+              </button>
+            </div>
           </div>
         )}
       </div>
